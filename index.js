b31819994964b74fdaba53a58e41089f04f2148c
Lemmy Mwaura <lemminton@gmail.com>
2022-01-16 15:23:43 +0300

    Added scroll reveal animations plus deleted unused files

--- index.js
+++ index.js
@@ -20,44 +20,69 @@ function linkAction() {
 }
 
 navLink.forEach((navlink) => {
-  navlink.addEventListener('click', linkAction)
+  navlink.addEventListener("click", linkAction)
 })
 
 // Scroll Sections Active link
-const sections = document.querySelectorAll('section[id]')
-function scrollActive(){
-  const scrollY = window.pageYOffset 
+const sections = document.querySelectorAll("section[id]")
+function scrollActive() {
+  const scrollY = window.pageYOffset
 
-  sections.forEach(current => {
+  sections.forEach((current) => {
     const sectionHeight = current.offsetHeight
-    const sectionTop = current.offsetTop - 50;
-    let sectionId = current.getAttribute('id')
+    const sectionTop = current.offsetTop - 50
+    let sectionId = current.getAttribute("id")
 
-    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document.querySelector('.navigation-menu a[href*=' + sectionId + ']').classList.add('active-link')
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".navigation-menu a[href*=" + sectionId + "]")
+        .classList.add("active-link")
     } else {
-      document.querySelector('.navigation-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+      document
+        .querySelector(".navigation-menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link")
     }
   })
 }
 
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive)
 
 // Change Background header
-function scrollHeader(){
-  const nav = document.getElementById('header')
-  if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
+function scrollHeader() {
+  const nav = document.getElementById("header")
+  if (this.scrollY >= 200) nav.classList.add("scroll-header")
+  else nav.classList.remove("scroll-header")
 }
 
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader)
 
 // Show scrolltop
-function scrollTop(){
-  const scrollTop = document.getElementById('scroll-top')
-  if(this.scrollY >= 560) scrollTop.classList.add('show-scrolltop'); else scrollTop.classList.remove('show-scrolltop');
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top")
+  if (this.scrollY >= 560) scrollTop.classList.add("show-scrolltop")
+  else scrollTop.classList.remove("show-scrolltop")
 }
 
-window.addEventListener('scroll', scrollTop)
+window.addEventListener("scroll", scrollTop)
+
+// Scroll Reveal animations
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 2000,
+  reset: true,
+})
+
+sr.reveal(
+  `.hero-data, .hero-container,
+            .about-data, .about-img,
+            .services-content, .menu-content,
+            .contact-data, .contact-button,
+            .footer-content`,
+  {
+    interval: 200,
+  }
+)
 
 class Pizza {
   constructor(size, price, toppings) {
